c666ad5cc4ae06eeaf69493d6f9dc799f69d2f7f
yuyu062 <chorong6544@naver.com>
2023-07-17 18:44:38 +0900

    최초 프로젝트 공유파일 과제

---
+++ src/main/java/com/mycompany/springhomework/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.mycompany.springhomework.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	//http://localhost:8080/springwebapp
+	@RequestMapping("/")
+	public String index() {
+		log.info("실행1");
+		return "index";
+	}
+}
